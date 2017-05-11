4faa259ec395a936bc67dd642339143664279115
David Mora <david.mora6162@gmail.com>
2017-05-11 07:03:01 -0600

    Adding Bot playground code

---
+++ BotApp/Dialogs/CardDialog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApp.Dialogs
+{
+    [Serializable]
+    public class CardDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> arg)
+        {
+            var replyToConversation = context.MakeMessage();
+
+            replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            replyToConversation.Attachments = new List<Attachment>();
+            List<CardImage> CardImages = new List<CardImage>();
+            CardImages.Add(new CardImage()
+            {
+                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/BMW-Z4_diagonal_front_at_IAA_2005.jpg/243px-BMW-Z4_diagonal_front_at_IAA_2005.jpg"
+            });
+
+            CardAction btnWebsite = new CardAction()
+            {
+                Type = "openUrl",
+                Title = "Open",
+                Value = "http://bmw.com"
+            };
+
+            HeroCard plCard = new HeroCard()
+            {
+                Title = $"Title",
+                Subtitle = $"Resultados de busqueda para",
+                Images = CardImages,
+                Tap = btnWebsite
+            };
+
+            var attachment = plCard.ToAttachment();
+            replyToConversation.Attachments.Add(attachment);
+            await context.PostAsync(replyToConversation);
+
+            //var connector = new ConnectorClient(new Uri(msg.ServiceUrl));
+            //var reply = connector.Conversations.SendToConversationAsync(replyToConversation);
+        }
+    }
+}
---
+++ BotApp/Dialogs/EchoDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApp.Dialogs
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> arg)
+        {
+            var message = await arg as IMessageActivity;
+            await context.PostAsync($"You said {message.Text}");
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
